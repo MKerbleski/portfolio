d14111635a3cf3b0510b32aabd168934b80863ae
mkerbleski <mkerbles@gmail.com>
2018-08-05 21:11:29 -0600

    Created title.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const AppDiv = styled.div`
+  border: 1px solid red;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const Header = styled.h1`
+  border: 1px solid blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  font-size: 13vw;
+`;
+
+const Mi = styled.h1`
+  color: white;
+  -webkit-text-stroke-color: black;
+  -webkit-text-stroke-width: 1px;
+  font-size: 13vw;
+
+`;
+
+const Ke = styled.h1`
+  color: gray;
+  font-size: 13vw;
+
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppDiv>
+        <Header><Mi>MI</Mi><Ke>KE</Ke>RBLESKI</Header>
+      </AppDiv>
     );
   }
 }
