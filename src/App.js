0b33e595cc0da8ddc79c328949fbb5defb6015b1
mkerbleski <mkerbles@gmail.com>
2018-08-11 13:01:56 -0600

    added main divs to index.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,49 @@ import './App.css';
 import {Route, Link} from 'react-router-dom'
 import styled from 'styled-components';
 
+class App extends Component {
+  render() {
+    return (
+
+      <AppDiv>
+        <div className="all-contact-tabs">
+          <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
+          <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
+          <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
+
+        </div>
+
+        <div className="main">
+
+          <Header><Mi>MI</Mi><Ke>KE</Ke>RBLESKI</Header>
+
+          <div className="links">
+            <Link to="/resume">resume</Link>
+            <Link to="/current-project">current-project</Link>
+            <Link to="/web-portfolio">web-portfolio</Link>
+            <Link to="/media-portfolio">media-portfolio</Link>
+          </div>
+
+        </div>
+
+        <footer>
+          <div>Copyright: Michael Kerbleski</div>
+          <div>Last Update:{}</div>
+        </footer>
+
+      </AppDiv>
+    );
+  }
+}
+
+export default App;
+
 const AppDiv = styled.div`
   border: 1px solid red;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space space-between;
   align-items: center;
   height: 100vh;
   margin: 0 auto;
@@ -37,28 +74,3 @@ const Ke = styled.h1`
   font-size: 13vw;
 
 `;
-
-class App extends Component {
-  render() {
-    return (
-
-      <AppDiv>
-        <div className="contact-tabs">
-          <img src="./img/twitter.svg" alt="twitter logo" />
-        </div>
-
-        <Header><Mi>MI</Mi><Ke>KE</Ke>RBLESKI</Header>
-
-        <div className="links">
-          <Link to="/resume">resume</Link>
-          <Link to="/current-project">current-project</Link>
-          <Link to="/web-portfolio">web-portfolio</Link>
-          <Link to="/media-portfolio">media-portfolio</Link>
-        </div>
-
-      </AppDiv>
-    );
-  }
-}
-
-export default App;
