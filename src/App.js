577d1e58abf52a7b3affb4920413f1318a8026c9
mkerbleski <mkerbles@gmail.com>
2018-08-12 16:13:32 -0600

    refactored to less

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
 import './css/index.css';
 import {Link} from 'react-router-dom'
-import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <div className="app-div">
-        <div className="all-contact-tabs">
-          <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
-          <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
-          <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
 
-        </div>
+        <header>
+          <div className="all-contact-tabs">
+            <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
+            <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
+            <img className="contact-tab" src="./img/twitter.svg" alt="twitter logo" />
+
+          </div>
+        </header>
+
 
         <div className="main">
-
-          <header className="header"><span>MIKERBLESKI</span></header>
-
-
-          <div className="links">
-            <Link to="/resume">resume</Link>
-            <Link to="/current-project">current-project</Link>
-            <Link to="/web-portfolio">web-portfolio</Link>
-            <Link to="/media-portfolio">media-portfolio</Link>
+          <div className="title"><h1><mi>MI</mi><ke>KE</ke>RBLESKI</h1></div>
+          <div className="all-links">
+            <Link className="link" to="/resume">resume</Link>
+            <Link className="link" to="/current-project">current-project</Link>
+            <Link className="link" to="/web-portfolio">web-portfolio</Link>
+            <Link className="link" to="/media-portfolio">media-portfolio</Link>
           </div>
-
         </div>
 
         <footer>
-          <div>Copyright: Michael Kerbleski</div>
-          <div>Last Update:{}</div>
+          <div className="copyright">Copyright: Michael Kerbleski</div>
+          <div className="last-update">Last Update:{}</div>
         </footer>
 
       </div>
@@ -40,16 +39,3 @@ class App extends Component {
 }
 
 export default App;
-
-const Mi = styled.h1`
-  color: white;
-  -webkit-text-stroke-color: black;
-  -webkit-text-stroke-width: 1px;
-  font-size: 13vw;
-
-`;
-
-const Ke = styled.h1`
-
-
-`;
