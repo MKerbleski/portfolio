9188d7f8b0afbba96e340f3da40d28160a76c6fd
mkerbleski <mkerbles@gmail.com>
2018-08-12 17:54:05 -0600

    created a components folder.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React, { Component } from 'react';
 import './css/index.css';
 import {Route, Link} from 'react-router-dom'
+import resume from './components/resume'
 
 class App extends Component {
   render() {
     return (
       <div className="app-div">
 
-        <header>
-          <div className="all-contact-tabs">
-            <div className="logo" href="https://angel.co/kerbleski"><i class="fab fa-angellist"></i></div>
-            <div className="logo" href="https://twitter.com/kerbleski"><i class="fab fa-twitter"></i></div>
-            <div className="logo" href="https://github.com/kerbleski"><i class="fab fa-github"></i></div>--
-            <div className="logo"><i class="far fa-moon"></i></div>
-            <div className="logo"><i class="far fa-sun"></i></div>
+        <Route path="/resume"  render={resume}></Route>
 
+        <Route path="/" exact >
+          <div className="home-div">
+            <header>
+              <div className="all-contact-tabs">
+                <div className="logo" href="https://angel.co/kerbleski"><i class="fab fa-angellist"></i></div>
+                <div className="logo" href="https://twitter.com/kerbleski"><i class="fab fa-twitter"></i></div>
+                <div className="logo" href="https://github.com/kerbleski"><i class="fab fa-github"></i></div>
+                --
+                <div className="logo">&#9728;</div>
+                <div className="logo">&#9790;</div>
+
+              </div>
+            </header>
+
+            <div className="main">
+              <div className="title"><h1><mi>MI</mi><ke>KE</ke>RBLE.SKI</h1></div>
+              <div className="all-links">
+                <Link className="link" to="/resume">resume</Link>
+                <Link className="link" to="/current-project">current-project</Link>
+                <Link className="link" to="/web-portfolio">web-portfolio</Link>
+                <Link className="link" to="/media-portfolio">media-portfolio</Link>
+              </div>
+            </div>
           </div>
-        </header>
 
-
-        <div className="main">
-          <div className="title"><h1><mi>MI</mi><ke>KE</ke>RBLESKI</h1></div>
-          <div className="all-links">
-            <Link className="link" to="/resume">resume</Link>
-            <Link className="link" to="/current-project">current-project</Link>
-            <Link className="link" to="/web-portfolio">web-portfolio</Link>
-            <Link className="link" to="/media-portfolio">media-portfolio</Link>
-          </div>
-        </div>
-
-        {/* <Route path="/">
-
-        </Route> */}
+        </Route>
 
         <footer>
           <div className="copyright">Copyright: Michael Kerbleski</div>
---
+++ src/components/resume.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const Resume = () => {
+  return (
+    <div className="resume-div">
+      <header>
+        <div className="all-contact-tabs">
+          <div className="logo" href="https://angel.co/kerbleski"><i class="fab fa-angellist"></i></div>
+          <div className="logo" href="https://twitter.com/kerbleski"><i class="fab fa-twitter"></i></div>
+          <div className="logo" href="https://github.com/kerbleski"><i class="fab fa-github"></i></div>--
+          <div className="logo">&#9728;</div>
+          <div className="logo">&#9790;</div>
+
+        </div>
+      </header>
+
+      <div>resume page</div>
+    </div>
+
+  )
+}
+
+export default Resume;
