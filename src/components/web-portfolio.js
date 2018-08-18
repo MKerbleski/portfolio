96a1f2885f18b8c0624106fd4f6ab5598a9b1252
mkerbleski <mkerbles@gmail.com>
2018-08-18 11:52:03 -0600

    worked on styling and display of web-portfolio.

--- src/components/web-portfolio.js
+++ src/components/web-portfolio.js
@@ -8,9 +8,15 @@ const PortfolioDiv = styled.div`
   border: 1px solid red;
   ${'' /* height: 100%; */}
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  .site-name {
+    background-color: gray;
+  }
+  .selected-site {
+    background-color: red;
+  }
   .site-div {
     display: flex;
     flex-direction: column;
@@ -28,30 +34,63 @@ const PortfolioDiv = styled.div`
 `;
 
 class WebPortfolio extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentProjectID: null,
+      currentProject: null,
+    }
+  }
+
   componentDidMount(){
    this.props.connnectGithub();
 
   }
+
+  selectSite = e => {
+    e.preventDefault();
+    console.log(this.props.state.githubData.data[e.target.id]);
+    this.setState({
+      currentProjectID: e.target.id,
+      currentProject: this.props.state.githubData.data,
+      //need to find a way to return the index of the array that matches the id
+    })
+  }
+
   render(){
-    console.log(this.props.state)
+    console.log(this.state.currentProject)
     return (
           <PortfolioDiv>
-            {(this.props.state.fetchedData === true) ?        (this.props.state.githubData.data.map( (project) => {
-              return (
-                <div className="site-div">
-                   <h1>{project.name}</h1>
-                   <p>{project.updated_at}</p>
-                   <p>description</p>
-                  {(project.has_pages === false) ? null : <iframe title={project.id} className="mini-site" src={`https://mkerbleski.github.io/${project.name}/`}></iframe> }
-                </div>
-              )
-            })) : null}
+            <div>
+              <h4>List of Projects</h4>
+              {(this.props.state.fetchedData === true) ?        (this.props.state.githubData.data.map( (project) => {
+                return (
+                  <div key={project.id} className={(this.state.currentProjectID == project.id) ? ('site-name selected-site') : ('site-name')} >
+                     <h1>{project.name}</h1>
+                     <p>{project.updated_at}</p>
+                     <p>{project.id}</p>
+                     <p>description</p>
+                     <p id={project.id} onClick={this.selectSite}>show</p>
+                    {/* {(project.has_pages === false) ? <div>no page enabled</div> : <iframe title={project.id} className="mini-site" src={`https://mkerbleski.github.io/${project.name}/`}></iframe> } */}
+                  </div>
+                )
+              })) : null}
+            </div>
+            <div>
+              <h4>site preview</h4>
+              {/* {
+                (this.state.currentProject !== null ) ? ( <div>{this.state.currentProject.name}</div>) : (<div>nope</div>)
+            } */}
+            </div>
+
+{/*
             <div className="site-div">
               <h1>title</h1>
               <p>date</p>
               <p>description</p>
               <iframe title="title" className="mini-site" src="https://mkerbleski.github.io/Sprint-Challenge-Applied-Javascript/"></iframe>
-            </div>
+            </div> */}
+
           </PortfolioDiv>
     )
   }
