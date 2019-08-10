077969ad85aab73842423fbb69d62febd72eb6b9
mkerbleski <mkerbles@gmail.com>
2019-08-10 17:58:25 -0400

    experimented with storing blog posts on a google sheet for convience

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -2,6 +2,7 @@ import React , { Component } from 'react'
 import styled from 'styled-components'
 import { Route, Link } from 'react-router-dom';
 import { withRouter } from 'react-router'
+import axios from 'axios'
 
 const blogPosts = {
     this: {
@@ -17,6 +18,9 @@ const blogPosts = {
     vrWorthEmulating: {
         link: "https://docs.google.com/document/d/e/2PACX-1vQUdPPAlzQE4sAmjj7OrxnEqW6nGTwIl7iEFTlOn1zc8Ymo7NKN-M8EqRfG7-qkO1nXLJBd3uPmULXd/pub",
     },
+    guess: {
+        link: "https://docs.google.com/document/d/e/2PACX-1vR9QN9kVs_6dqlhePT4H888m9eqsqdpyS5xWd4v9LTrmBUdiYSwjeXh6OgBX9ydnJt-9__ThQNx7pOY/pub",
+    },
 }
 
 class BlogPage extends Component {
@@ -26,25 +30,38 @@ class BlogPage extends Component {
         }
     }
 
+    componentDidMount(){
+        axios.get('https://sheetsu.com/apis/v1.0bu/2300c8af088e').then(res => {
+            this.setState({
+                blogPosts: res.data
+            })
+        })
+    }
+
     render(){
-        console.log('this.props', this.props)
+        console.log('this.props', this.state)
         return(
             <BlogPageDiv> 
-            <ul className="blogLinks">
-                <h4>Posts</h4>  
-
-                {Object.keys(blogPosts).map(key => {
-                    return (
-                        <li className="blogLink">
-                            {this.props.location.pathname.includes(key) ? '-> ' : null}<Link to={`/blog/${key}`} >{key}</Link>
-                        </li>)
-                })}
-            </ul>
-                <Route path={`/blog/:postTitle`} render={(a) => 
-                <div className="wholePost">
-                    <iframe className="post" src={blogPosts[a.match.params.postTitle].link} ></iframe>
-                </div>} />
-
+            {this.state.blogPosts 
+                    ?
+                    <React.Fragment>
+                        <ul className="blogLinks">
+                            <h4>Posts</h4>  
+                            {this.state.blogPosts.map(key => {
+                                return (
+                                    <li className="blogLink">
+                                        {this.props.location.pathname.includes(key.keyword) ? '-> ' : null}<Link to={`/blog/${key.keyword}`} >{key.keyword}</Link>
+                                    </li>)
+                            })} 
+                            
+                        
+                        </ul>
+                            <Route path={`/blog/:postTitle`} render={(a) => 
+                            <div className="wholePost">
+                                <iframe className="post" src={this.state.blogPosts.filter(each => each.keyword === a.match.params.postTitle)[0].url} ></iframe>
+                            </div>} />
+                    </React.Fragment>
+                : null}
 
             </BlogPageDiv>
         )
